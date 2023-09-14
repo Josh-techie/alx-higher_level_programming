92d1a0557653fe6166abb2344213b6707694b129
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-09-15 00:59:25 +0100

    2-Arg second method

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,16 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
+// Access command line arguments (excluding the first two)
+const args = process.argv.slice(2);
+
+// Check the number of arguments
+if (args.length === 0) {
+  console.log("No argument");
+} 
+else if (args.length === 1 ){
+  console.log("Argument found");
 }
+else {
+  console.log("Arguments found");
+}
+
+
