b791ad6f4466f7b10ad08c230ca7f6c82ea17cfe
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-09-15 01:43:45 +0100

    9.Add with 2 methods

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,5 +2,15 @@
 function add (a, b) {
   return (a + b);
 }
+// First methode using Number function
+// console.log(add(Number(process.argv[2]), Number(process.argv[3])));
 
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+// Second methode
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  console.log(add(num1, num2));
+}
