7bb5546053be62710dd033c99510ca4c024338ef
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-09-15 01:17:19 +0100

    Loop task-6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const arrS = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < arrS.length; i++) {
-  console.log(arrS[i]);
+
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (const language of languages) {
+  console.log(language);
 }
