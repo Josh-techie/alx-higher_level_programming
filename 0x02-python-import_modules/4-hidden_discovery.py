c074a615cf471f456b297cd6eed707942def944a
Josh-techie <youse2002@outlook.fr>
2023-11-03 21:31:51 +0100

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    arr = dir(hidden_4)
-    for i in range(len(arr)):
-        if (arr[i][:2] != "__"):
-            print(arr[i])
+    data = dir(hidden_4)
+    for i in range(len(data)):
+        if (data[i][:2] != "__"):
+            print(data[i])
